5b456d68e591f8e9d6ef46d66bca09a0841c021b
Elias Alberto Alegre Barazorda <betoab728@gmail.com>
2024-12-04 21:44:25 -0500

    base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/components/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    //se carga el login en primer lugar
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    //se carga el login
+
+    { path: 'login', component: LoginComponent },
+
+];
